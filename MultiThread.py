d4379f43821a97609f77e1ea98eea11bd0f771ea
rajmuni <rajmuni1337@gmail.com>
2021-04-22 09:55:33 +0530

    removed commit in three lines

--- MultiThread.py
+++ MultiThread.py
@@ -5,7 +5,6 @@ t=Thread(target=display)
 t.start()
 print('This code executed by Thread:',current_thread().getName())
 
-
-#C:\Users\Raj Muni\Desktop>py test.py
-#This code (display function) executed by thread: Thread-1
-#This code executed by Thread: MainThread
+C:\Users\Raj Muni\Desktop>py test.py
+This code (display function) executed by thread: Thread-1
+This code executed by Thread: MainThread
