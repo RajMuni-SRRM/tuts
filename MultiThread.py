40e58a93c10969fcc047fd38b57c15d22cbd109e
RajMuni-SRRM <67454746+RajMuni-SRRM@users.noreply.github.com>
2021-04-22 10:27:42 +0530

    changes happned from git hub

--- MultiThread.py
+++ MultiThread.py
@@ -1,9 +1,9 @@
 from threading import *
 def display():
 	print('This code (display function) executed by thread:',current_thread().getName())
-t=Thread(target=display)
-t.start()
-print('This code executed by Thread:',current_thread().getName())
+#t=Thread(target=display)
+#t.start()
+#print('This code executed by Thread:',current_thread().getName())
 
 C:\Users\Raj Muni\Desktop>py test.py
 This code (display function) executed by thread: Thread-1
