824910d2555ba570c74bf8d6330f7336ae81e6ea
RajMuni-SRRM <rajmuni1337@gmai.com>
2022-10-08 11:47:58 +0530

    test.py file modified

--- test.py
+++ test.py
@@ -1,16 +1,4 @@
 Adding new files for testing different repos inro
-<<<<<<< HEAD
-<<<<<<< HEAD
-changes are doen from massssssssssssssttttttttterrrrrrrr
-=======
 chnages are done from sprint1
->>>>>>> sprint1
-||||||| 17ae624
 chnages are done from sprint1
-=======
-
-chnages are done from spriiiiiiiiiinnnnnnnnnnnntttttttttt1
->>>>>>> sprint1
 changes happened from git hub
-changes happened from o\the ubdsvvuds dhdv pre-pord branch
-changes done from pre prod
